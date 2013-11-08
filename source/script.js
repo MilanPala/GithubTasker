37f63ef477a15d9f5735d859a06be5bc6eda7d8e
Milan Pála <milan.pala@email.cz>
2013-11-08 23:05:56 +0100

    Úkoly se aktulizují jednou za 10 sekund a stav úkolů se zobrazuje v title

--- source/script.js
+++ source/script.js
@@ -1,3 +1,43 @@
+function loadTasks($tasker, $checkbox) {
+	var checked = 0;
+	var total = 0;
+	$content.empty();
+	$('.discussion-bubble').each(function() {
+		var $comment = $(this);
+		$(this).find('.task-list').each(function(){
+			checked += $(this).find('.task-list-item-checkbox:checked').length;
+			total += $(this).find('.task-list-item-checkbox').length;
+			var $header = $comment.find('comment-header').clone();
+			$content.append($header);
+			$ul = $(this).clone();
+			$ul.find('.task-list-item-checkbox').css('margin-top', '2px');
+			$ul.css('font-size', '13px');
+			$ul.css('line-height', '20px');
+			$ul.css('cursor', 'help');
+			$ul.find(':checkbox').prop('disabled', true).click(function() {return false;});
+			$ul.click(function() {
+				$('html, body').animate({
+					scrollTop: $comment.offset().top
+				}, 500);
+			});
+			$content.append($ul);
+		});
+	});
+	$tasker.find('#github-tasker-progress').empty();
+	$tasker.find('#github-tasker-progress').append(' '+checked+'/'+total);
+	patt=/([0-9]+\/[0-9]+ )(.+)/;
+	var title = document.title;
+	title = title.replace(patt, '$2');
+	document.title = checked+'/'+total+' '+title;
+	if(total) {
+		$content.css('height', '300px');
+	}
+
+	var $checkboxes = $tasker.find('.task-list li input[type="checkbox"]:checked').closest('li');
+	$checkboxes.toggle($checkbox.is(':checked'));
+	setTimeout(loadTasks, 10000, $tasker, $checkbox);
+}
+
 $(function() {
 	$tasker = $('<div id="github-tasker" class="markdown-body"><div style="border-style: solid; border-color: #CACACA; border-width: 1px 1px 0 1px;"><h2 style="height: 30px; line-height: 30px; margin: 0; font-size: 21px; padding: 0 5px; position: relative; cursor: pointer; background-color: #e1e1e1; background-image: -webkit-linear-gradient(#f8f8f8, #e1e1e1); border-bottom: 1px solid #ccc;">Github Tasker <span id="github-tasker-progress"></span> <input type="checkbox" id="github-tasker-notcheckedonly" style="position: absolute; right: 3px; width: 20px; height: 20px; top: 5px;" /></h2><div id="github-tasker-content"></div></div></div>');
 	$tasker.css('position', 'fixed');
@@ -39,37 +79,7 @@ $(function() {
 		$.cookie('github-tasker-show', !show, { expires: 100 });
 	});
 	
-	var checked = 0;
-	var total = 0;
-	$('.discussion-bubble').each(function() {
-		var $comment = $(this);
-		$(this).find('.task-list').each(function(){
-			checked += $(this).find('.task-list-item-checkbox:checked').length;
-			total += $(this).find('.task-list-item-checkbox').length;
-			var $header = $comment.find('comment-header').clone();
-			$content.append($header);
-			$ul = $(this).clone();
-			$ul.find('.task-list-item-checkbox').css('margin-top', '2px');
-			$ul.css('font-size', '13px');
-			$ul.css('line-height', '20px');
-			$ul.css('cursor', 'help');
-			$ul.find(':checkbox').prop('disabled', true).click(function() {return false;});
-			$ul.click(function() {
-				$('html, body').animate({
-					scrollTop: $comment.offset().top
-				}, 500);
-			});
-			$content.append($ul);
-		});
-	});
-	$tasker.find('#github-tasker-progress').append(' '+checked+'/'+total);
-	if(total) {
-		$content.css('height', '300px');
-	}
-
-	var $checkboxes = $tasker.find('.task-list li input[type="checkbox"]:checked').closest('li');
-	$checkboxes.toggle($checkbox.is(':checked'));
+	loadTasks($tasker, $checkbox);
 	
 	$('body').append($tasker);
-	
 });
